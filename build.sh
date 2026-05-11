be5863999a1a3dbe6396f52c18a8d0d66b110df5
rinnsakaguchi <ffrizz042@gmail.com>
2026-05-12 02:14:40 +0700

    builder: drop ksu clean

--- build.sh
+++ build.sh
@@ -170,11 +170,6 @@ DEFCONFIG="$DEFCONFIG_FILE"
 # Clean KSU
 sed -i '/CONFIG_KSU/d' "$DEFCONFIG"
 sed -i '/CONFIG_KSU_SUSFS/d' "$DEFCONFIG"
-sed -i '/source "drivers/kernelsu/Kconfig"/d' "drivers/Kconfig"
-sed -i '/obj-$(CONFIG_KSU) += kernelsu//d' "drivers/Makefile"
-
-rm -rf KernelSU
-rm -rf drivers/kernelsu
 
 if [ "$VARIANT" == "KSU" ] || [ "$VARIANT" == "SUSFS" ]; then
     # Patch KernelSU-Next
